0632ceede000c1447104b1407002c53727db7a69
allikumar <121488517+allikumar@users.noreply.github.com>
2023-01-11 17:07:03 +0530

    Update ProductRepository.java

--- Product/src/main/java/com/product/repository/ProductRepository.java
+++ Product/src/main/java/com/product/repository/ProductRepository.java
@@ -1,13 +1,13 @@
-package com.product.repository;
+package com.task.Product.repo;
 
 import org.springframework.data.mongodb.repository.MongoRepository;
 import org.springframework.stereotype.Repository;
 
-import com.product.entity.Product;
-
-
+import com.task.Product.entity.Product;
 
 @Repository
-public interface ProductRepository extends MongoRepository<Product, String> {
+public interface ProductRepo extends MongoRepository<Product,Integer> {
+
+	
 
 }
