ea1071ceffe567c8f8b777a3242331bd662bb32e
allikumar <121488517+allikumar@users.noreply.github.com>
2023-01-11 17:07:49 +0530

    Update ProductService.java

--- Product/src/main/java/com/product/service/ProductService.java
+++ Product/src/main/java/com/product/service/ProductService.java
@@ -1,19 +1,15 @@
-package com.product.service;
+package com.task.Product.service;
 
 import java.util.List;
 
-import com.product.entity.Product;
+import com.task.Product.entity.Product;
 
 public interface ProductService {
-	
-	public String addProduct(Product product);
 
-	public String updateProduct(String id, Product product);
-
-	public String deleteProduct(String id);
-
-	public Product findProduct(String id);
-
-	public List<Product> findAllProduct();
-
+public String addProduct(Product product);
+public List<Product> findallProducts();
+public Product findProduct(int id);
+public String deleteProduct(int id);
+public String updateProduct(int id,Product product);
 }
+
