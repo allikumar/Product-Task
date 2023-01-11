82a47a333ce64eb2fbc74630bb483751a85f8b88
allikumar <121488517+allikumar@users.noreply.github.com>
2023-01-11 17:02:47 +0530

    Update ProductApplicationTests.java

--- Product/src/test/java/com/product/test/ProductApplicationTests.java
+++ Product/src/test/java/com/product/test/ProductApplicationTests.java
@@ -1,49 +1,53 @@
-package com.product.test;
+package com.task.Product;
 
-import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
-//import java.util.stream.Collectors;
-//import java.util.stream.Stream;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 
-import org.assertj.core.api.Assertions;
-import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 
-import com.product.entity.Product;
-import com.product.project.ProductApplication;
-import com.product.repository.ProductRepository;
-import com.product.service.ProductService;
+import com.task.Product.entity.Product;
+import com.task.Product.repo.ProductRepo;
+import com.task.Product.service.ProductService;
 
-
-
-@SpringBootTest(classes = ProductApplication.class, webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@SpringBootTest
 class ProductApplicationTests {
-	
-	@Autowired
-	private ProductService productService;
-	
-	@MockBean
-	private ProductRepository productRepository;
-	
-	@Test
-	@DisplayName("Add Product Test")
-	public void addProductTest() {
-		Product product=new Product("1028","Smart TV", "Grey-Black", "55000", "VEIRA Electronics", "Infinix" );
-		when(productRepository.save(product)).thenReturn(product);
-		Assertions.assertThat(product.getId()).hasSizeGreaterThan(0);
-	}
 
+	
+	
 	@Test
-	@DisplayName("Delete Product Test")
-	public void DeleteProductTest() {
-		Product product=new Product("1028","Smart TV", "Grey-Black", "55000", "VEIRA Electronics", "Infinix");
-		productService.deleteProduct(product.getId());
-		assertEquals(0,productService.findAllProduct().size());
+	void contextLoads() {
 	}
+	@MockBean
+	private ProductRepo productRepository;
+		@Autowired
+	private ProductService productService;
+		
+	@Test
+		public void getAllTest() {
+			when(productRepository.findAll()).thenReturn(Stream.of(new Product(1,"screw",2,"Other",500),
+					new Product(1,"screw",2,"Other",500)).collect(Collectors.toList()));
+			assertEquals(2, productService.findallProducts().size());
+		}
+	
+	
+	
+	@Test
+	public void deleteTest() {
+		Product product = new Product(1,"screw",2,"Other",500);
+		productService.deleteProduct(1);
+		verify(productRepository,times(1)).deleteById(1);
+	}
+	
+
 
 }
+
