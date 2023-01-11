a46e1a6de66e8cd05e76b5372ebadd88f7ea7d26
allikumar <121488517+allikumar@users.noreply.github.com>
2023-01-11 17:06:15 +0530

    Update ProductApplication.java

--- Product/src/main/java/com/product/project/ProductApplication.java
+++ Product/src/main/java/com/product/project/ProductApplication.java
@@ -1,15 +1,15 @@
-package com.product.project;
-
+package com.task.Product;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceTransactionManagerAutoConfiguration;
-
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+@EnableDiscoveryClient
 @SpringBootApplication(exclude = { DataSourceAutoConfiguration.class, DataSourceTransactionManagerAutoConfiguration.class})
 public class ProductApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(ProductApplication.class, args);
 	}
-
 }
