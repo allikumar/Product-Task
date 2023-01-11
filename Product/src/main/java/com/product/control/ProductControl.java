9ab5a6617b171db39913cebd5cef0c162218afa3
allikumar <121488517+allikumar@users.noreply.github.com>
2023-01-11 17:04:10 +0530

    Update ProductControl.java

--- Product/src/main/java/com/product/control/ProductControl.java
+++ Product/src/main/java/com/product/control/ProductControl.java
@@ -1,48 +1,54 @@
-package com.product.control;
-
-
+package com.task.Product.controller;
 
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-
-
-import com.product.entity.Product;
-
-import com.product.service.ProductService;
-
+import com.task.Product.entity.Product;
+import com.task.Product.service.ProductService;
 @RestController
 @RequestMapping("/product")
-public class ProductControl {
+
+public class ProductController {
 	
 	@Autowired
-	ProductService productService;
-
-	@PostMapping("/addproduct")
+	private ProductService productService;
+	
+	@PostMapping("/add")
 	public String addProduct(@RequestBody Product product) {
-		return productService.addProduct(product);
+		return productService.addProduct(product) ;
+		
 	}
-
-	@PutMapping("/updateproduct/{id}")
-	public String updateProduct(@PathVariable String id, @RequestBody Product product) {
+	
+	@GetMapping("/findall")
+	public List<Product> findAllProducts(){
+		return productService.findallProducts();
+	}
+	
+	@GetMapping("/find/{id}")
+	public Product findProduct(@PathVariable int id) {
+		return productService.findProduct(id);
+	}
+	
+	@PutMapping("/update/{id}")
+	public String updateProduct(@PathVariable int id, @RequestBody Product product) {
 		return productService.updateProduct(id, product);
 	}
-
-	@DeleteMapping("/deleteproduct/{id}")
-	public String deleteProduct(@PathVariable String id) {
+	
+	@DeleteMapping("/delete/{id}")
+	public String deleteProduct(@PathVariable int id) {
 		return productService.deleteProduct(id);
 	}
 
-	@GetMapping("/findproduct/{id}")
-	public Product findProduct(@PathVariable String id) {
-		return productService.findProduct(id);
-	}
 
-	@GetMapping("/findallproduct")
-	public List<Product> findAllProduct() {
-		return productService.findAllProduct();
-	}
-
+	
+	
 }
