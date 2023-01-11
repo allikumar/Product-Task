7cf760da0e4db60914794b23f70a464f6984eeaa
allikumar <121488517+allikumar@users.noreply.github.com>
2023-01-11 17:05:31 +0530

    Update Product.java

--- Product/src/main/java/com/product/entity/Product.java
+++ Product/src/main/java/com/product/entity/Product.java
@@ -1,69 +1,71 @@
-package com.product.entity;
+package com.task.Product.entity;
 
-import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.annotation.Id;
+
+
 
 @Document(collection="Product-Details")
 public class Product {
-	
-	@Id
-	private String id;
-	private String productname;
-	private String productcolor;
-	private String productprice;
-	private String productseller ;
-	private String productbrand ;
-	
-	public String getId() {
-		return id;
-	}
-	public void setId(String id) {
-		this.id = id;
-	}
-	public String getProductName() {
-		return productname;
-	}
-	public void setProductName(String productname) {
-		this.productname = productname;
-	}
-	public String getProductColor() {
-		return productcolor;
-	}
-	public void setProductColor(String productcolor) {
-		this.productcolor = productcolor;
-	}
-	public String getProductPrice() {
-		return productprice;
-	}
-	public void setProductPrice(String productprice) {
-		this.productprice = productprice;
-	}
-	public String getProductSeller() {
-		return productseller;
-	}
-	public void setProductSeller(String productseller) {
-		this.productseller = productseller;
-	}
-	public String getProductBrand() {
-		return productbrand;
-	}
-	public void setProductBrand(String productbrand) {
-		this.productbrand = productbrand;
-	}
-	
-	public Product(String id, String productname, String productcolor, String productprice, String productseller, String productbrand) {
-		super();
-		this.id = id;
-		this.productname = productname;
-		this.productcolor = productcolor;
-		this.productprice = productprice;
-		this.productseller = productseller;
-		this.productbrand = productbrand;
-		
-	}
-	public Product() {
-		super();
-		// TODO Auto-generated constructor stub
-	}
+@Id	
+private int id;
+private String product_name;
+private int quantity;
+private String description;
+private double price;
+
+public Product() {
+	super();
+}
+
+public Product(int id, String product_name, int quantity, String description, double price) {
+	super();
+	this.id = id;
+	this.product_name = product_name;
+	this.quantity = quantity;
+	this.description = description;
+	this.price = price;
+}
+
+public int getId() {
+	return id;
+}
+
+public void setId(int id) {
+	this.id = id;
+}
+
+public String getProduct_name() {
+	return product_name;
+}
+
+public void setProduct_name(String product_name) {
+	this.product_name = product_name;
+}
+
+public int getQuantity() {
+	return quantity;
+}
+
+public void setQuantity(int quantity) {
+	this.quantity = quantity;
+}
+
+public String getDescription() {
+	return description;
+}
+
+public void setDescription(String description) {
+	this.description = description;
+}
+
+public double getPrice() {
+	return price;
+}
+
+public void setPrice(double price) {
+	this.price = price;
+}
+
 
 }
